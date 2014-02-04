b25f160e7d06f37c552ff83e1adb310e6286c25d
Matt Barlow <matt.barlow@rackspace.com>
2014-02-04 06:15:18 -0600

    Refactor regex function and convert datetime

--- orgify.py
+++ orgify.py
@@ -4,19 +4,23 @@ import re
 import sys
 import os
 import pdb
+import datetime
+import time
 
-def split_lines(txtfile):
-    pattern = re.compile(r'(^Date: )(.*)')
-    match = pattern.match(txtfile)
-    date = match.group(2)
-
-    pattern = re.compile(r'(^Topic: )(.*)', re.M)
+def match_headers(txtfile, match_string):
+    pattern = re.compile(r'(^{}: )(.*)'.format(match_string), re.M)
     match = pattern.search(txtfile)
-    topic = match.group(2)
+    content = match.group(2)
 
-    print "Date is {} and Topic is {}".format(date, topic)
+    return content
+
+def convert_date(journal_date):
+    converted_time = time.strptime(journal_date, "%B %d, %Y %I:%M %p")
+    return time.strftime("<%Y-%m-%d %a>", converted_time)
+    
 
 if __name__ == "__main__":
     journalfile = open('journal01.txt', 'r')
     txt = journalfile.read()
-    split_lines(txt)
+    print convert_date(match_headers(txt, "Date"))
+    print match_headers(txt, "Topic")
